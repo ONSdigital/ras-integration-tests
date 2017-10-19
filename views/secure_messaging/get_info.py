ef1dccd7935732d52c2c5dc217059040730a70ea
Rhiannon Griffiths <rhiannon.griffiths@ons.gov.uk>
2017-10-19 12:26:12 +0100

    setting up repo

---
+++ views/secure_messaging/get_info.py
@@ -0,0 +1,26 @@
+import nose
+from behave import given, then
+from flask import json
+from app.application import app
+
+
+@given('the user requests endpoint info')
+def step_impl_requests_endpoint_info(context):
+    context.response = app.test_client().get('/info')
+
+
+@then('the endpoint info is returned')
+def step_impl_endpoint_info_is_returned(context):
+    response = json.loads(context.response.data)
+    nose.tools.assert_equal(response['name'], 'ras-secure-message')
+    nose.tools.assert_equal(response['version'], '0.1.0')
+    # nose.tools.assert_equal(response['origin'], 'https://github.com/ONSdigital/ras-secure-message.git')
+    # nose.tools.assert_equal(response['commit'], 'not specified')
+    # nose.tools.assert_equal(response['branch'], 'not specified')
+    # nose.tools.assert_equal(response['built'], '01-01-1900 00:00:00.000')
+
+
+@then('a success status code (200) is returned')
+def step_impl_success_returned(context):
+    """validate that the status code was 200"""
+    nose.tools.assert_equal(context.response.status_code, 200)
