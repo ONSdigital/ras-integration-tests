4cbfdda98c5eaa55a36f01502c8416e04b81b11f
Rhiannon Griffiths <rhiannon.griffiths@ons.gov.uk>
2017-10-26 14:56:32 +0100

    renaming success function

--- views/features/steps/common.py
+++ views/features/steps/common.py
@@ -3,5 +3,5 @@ from behave import then
 
 
 @then('a success status code (200) is returned')
-def step_impl_success_returned(context):
+def success_returned(context):
     nose.tools.assert_equal(context.response.status_code, 200)
