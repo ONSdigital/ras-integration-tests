d2173ad0da5ae412263bd12c9300ecbd6e6a8eba
Andrew Millar <andrew.millar@qa.com>
2018-01-05 10:26:51 +0000

    Update working of steps to be in line with user story

--- acceptance_tests/features/steps/view_collection_exercise.py
+++ acceptance_tests/features/steps/view_collection_exercise.py
@@ -3,17 +3,17 @@ from behave import given, when, then
 from acceptance_tests.features.pages import collection_exercise
 
 
-@given('the 2017 collection exercise for the BRES survey exists')
+@given('the 2017 collection exercise for the BRES survey has been created')
 def bres_2017_exists(_):
     pass
 
 
-@when('the internal user views the 2017 collection exercise for the BRES survey')
+@when('the internal user navigates to the collection exercise details page')
 def internal_user_views_2017_bres_collection_exercise(_):
     collection_exercise.go_to('bres', 'BRES_2017')
 
 
-@then('the internal user can view the collection exercise details')
+@then('the user is able to view the survey details and period for that survey')
 def internal_user_can_view_bres_2017_collection_exercise_details(_):
     survey_info = collection_exercise.get_survey_info()
     assert survey_info == "221 - Business Register and Employment Survey (BRES)"
--- acceptance_tests/features/view_collection_exercise.feature
+++ acceptance_tests/features/view_collection_exercise.feature
@@ -3,7 +3,7 @@ Feature: View collection exercise
   I need to view the details for a single collection exercise
   So that I am assured the collection exercise are correct
 
-  Scenario: View attributes for a single collection exercise
-    Given the 2017 collection exercise for the BRES survey exists
-    When the internal user views the 2017 collection exercise for the BRES survey
-    Then the internal user can view the collection exercise details
+  Scenario: View collection exercise details
+    Given the 2017 collection exercise for the BRES survey has been created
+    When the internal user navigates to the collection exercise details page
+    Then the user is able to view the survey details and period for that survey
