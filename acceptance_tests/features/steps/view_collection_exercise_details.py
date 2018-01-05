605d7b137b197bc0ad1de111fe53628ff0a7e86f
Andrew Millar <andrew.millar@qa.com>
2018-01-05 11:04:21 +0000

    Update file names so not to clash with other PR

--- acceptance_tests/features/steps/view_collection_exercise_details.py
+++ acceptance_tests/features/steps/view_collection_exercise_details.py
@@ -1,6 +1,6 @@
 from behave import given, when, then
 
-from acceptance_tests.features.pages import collection_exercise
+from acceptance_tests.features.pages import collection_exercise_details
 
 
 @given('the 2017 collection exercise for the BRES survey has been created')
@@ -10,14 +10,14 @@ def bres_2017_exists(_):
 
 @when('the internal user navigates to the collection exercise details page')
 def internal_user_views_2017_bres_collection_exercise(_):
-    collection_exercise.go_to('bres', 'BRES_2017')
+    collection_exercise_details.go_to('bres', 'BRES_2017')
 
 
 @then('the user is able to view the survey details and period for that survey')
 def internal_user_can_view_bres_2017_collection_exercise_details(_):
-    survey_info = collection_exercise.get_survey_info()
+    survey_info = collection_exercise_details.get_survey_info()
     assert survey_info == "221 - Business Register and Employment Survey (BRES)"
-    period = collection_exercise.get_period()
+    period = collection_exercise_details.get_period()
     assert period == "BRES_2017"
-    user_description = collection_exercise.get_user_description()
+    user_description = collection_exercise_details.get_user_description()
     assert user_description == "August 2017"
--- acceptance_tests/features/view_collection_exercise_details.feature
+++ acceptance_tests/features/view_collection_exercise_details.feature
@@ -1,7 +1,7 @@
-Feature: View collection exercise
+Feature: View collection exercise details
   As a Collection Exercise Coordinator
   I need to view the details for a single collection exercise
-  So that I am assured the collection exercise are correct
+  So that I am assured the collection exercise is correct
 
   Scenario: View collection exercise details
     Given the 2017 collection exercise for the BRES survey has been created
