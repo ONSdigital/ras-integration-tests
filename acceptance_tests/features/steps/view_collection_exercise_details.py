f418f09787acf4765b34cea2e3da4b0c1eead039
Andrew Millar <andrew.millar@qa.com>
2018-01-10 13:02:29 +0000

    Use QBS 1803 for collection exercise details page

--- acceptance_tests/features/steps/view_collection_exercise_details.py
+++ acceptance_tests/features/steps/view_collection_exercise_details.py
@@ -3,19 +3,19 @@ from behave import given, when, then
 from acceptance_tests.features.pages import collection_exercise_details
 
 
-@given('the 2017 collection exercise for the BRES survey has been created')
+@given('the 1803 collection exercise for the QBS survey has been created')
 def bres_2017_exists(_):
     pass
 
 
 @when('the internal user navigates to the collection exercise details page')
 def internal_user_views_2017_bres_collection_exercise(_):
-    collection_exercise_details.go_to('bres', 'BRES_2017')
+    collection_exercise_details.go_to('QBS', '1803')
 
 
 @then('the user is able to view the survey details and period for that survey')
 def internal_user_can_view_bres_2017_collection_exercise_details(_):
     ce_details = collection_exercise_details.get_collection_exercise_details()
-    assert ce_details['survey_info'] == "221 - Business Register and Employment Survey (BRES)"
-    assert ce_details['period'] == "BRES_2017"
-    assert ce_details['user_description'] == "August 2017"
+    assert ce_details['survey_info'] == "139 - Quarterly Business Survey (QBS)"
+    assert ce_details['period'] == "1803"
+    assert ce_details['user_description'] == "9 March 2018"
--- acceptance_tests/features/view_collection_exercise_details.feature
+++ acceptance_tests/features/view_collection_exercise_details.feature
@@ -4,6 +4,6 @@ Feature: View collection exercise details
   So that I am assured the collection exercise is correct
 
   Scenario: View collection exercise details
-    Given the 2017 collection exercise for the BRES survey has been created
+    Given the 1803 collection exercise for the QBS survey has been created
     When the internal user navigates to the collection exercise details page
     Then the user is able to view the survey details and period for that survey
