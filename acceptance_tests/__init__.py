699e8e196ee0cf38f28822c7521c6471d32efa48
Andrew Millar <andrew.millar@qa.com>
2018-01-22 16:42:50 +0000

    Replace phantomjs browser with headless chrome
    PhantomJS is now deprecated in favour of headless chrome

--- acceptance_tests/__init__.py
+++ acceptance_tests/__init__.py
@@ -3,6 +3,6 @@ import os
 from splinter import Browser
 
 if os.getenv('HEADLESS', 'True') == 'True':
-    browser = Browser('phantomjs')
+    browser = Browser('chrome', headless=True)
 else:
     browser = Browser('chrome')
