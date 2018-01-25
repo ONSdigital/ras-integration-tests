31957473b419e5396ac773f8410469972b4b5eeb
Andrew Millar <andrew.millar@qa.com>
2018-01-25 10:34:57 +0000

    Update event dates

--- acceptance_tests/features/steps/view_collection_exercise_details.py
+++ acceptance_tests/features/steps/view_collection_exercise_details.py
@@ -30,8 +30,8 @@ def internal_user_can_view_bres_2017_collection_exercise_details(_):
 @then('the user is able to view the event dates for that collection exercise')
 def internal_user_can_view_bres_2017_collection_exercise_events(_):
     ce_events = collection_exercise_details.get_collection_exercise_events()
-    assert ce_events['mps'] == "Wed 14 Mar 2018 00:00 GMT"
-    assert ce_events['go_live'] == "Mon 19 Mar 2018 00:00 GMT"
-    assert ce_events['return_by'] == "Sat 27 Mar 2018 00:00 GMT"
-    assert ce_events['first_reminder'] == "Tue 03 Apr 2018 00:00 GMT"
-    assert ce_events['exercise_end'] == "Thu 30 Apr 2020 00:00 GMT"
+    assert ce_events['mps'] == "Wednesday 14 Mar 2018 00:00 GMT"
+    assert ce_events['go_live'] == "Monday 19 Mar 2018 00:00 GMT"
+    assert ce_events['return_by'] == "Tuesday 27 Mar 2018 00:00 GMT"
+    assert ce_events['first_reminder'] == "Tuesday 08 May 2018 00:00 GMT"
+    assert ce_events['exercise_end'] == "Thursday 30 Apr 2020 00:00 GMT"
