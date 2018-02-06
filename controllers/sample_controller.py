792b58f0452c8637367ec99002e96a9782a83e00
Andrew Millar <andrew.millar@qa.com>
2018-02-06 17:07:24 +0000

    Use f-string in load_sample

--- controllers/sample_controller.py
+++ controllers/sample_controller.py
@@ -5,6 +5,6 @@ from config import Config
 
 
 def load_sample(survey_name, period, test_file):
-    browser.visit('{}/surveys/{}/{}'.format(Config.RESPONSE_OPERATIONS_UI, survey_name, period))
+    browser.visit(f'{Config.RESPONSE_OPERATIONS_UI}/surveys/{survey_name}/{period}')
     browser.driver.find_element_by_id('sampleFile').send_keys(abspath(test_file))
     browser.find_by_id('btn-load-sample').click()
