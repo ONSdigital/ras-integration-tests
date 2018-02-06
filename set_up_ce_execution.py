a3fe65aeaaacbba2f77e2ba31d29bcf6a9228ba8
Andrew Millar <andrew.millar@qa.com>
2018-02-06 13:16:22 +0000

    Fix linting errors

--- set_up_ce_execution.py
+++ set_up_ce_execution.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
     # Add data required to execute collection exercise
     database_controller.execute_rm_sql('resources/database/set_up_ce_execute.sql')
     # Retrieve collection exercise for BRICKS 2018 and upload collection instrument for it
-    bricks_2018_ce = collection_exercise_controller.get_collection_exercise('cb8accda-6118-4d3b-85a3-149e28960c54', '201801')
-    collection_instrument_controller.upload_collection_instrument(bricks_2018_ce['id'], 'resources/collection_instrument_files/064_0001_201803.xlsx')
+    bricks_2018_ce = collection_exercise_controller.get_collection_exercise('cb8accda-6118-4d3b-85a3-149e28960c54',
+                                                                            '201801')
+    ci_path = 'resources/collection_instrument_files/064_0001_201803.xlsx'
+    collection_instrument_controller.upload_collection_instrument(bricks_2018_ce['id'], ci_path)
     logging.info(f"Collection exercise {bricks_2018_ce['id']} can be executed")
