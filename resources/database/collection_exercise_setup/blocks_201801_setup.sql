0085015ba797a5d1f3ef0212153ce8ab43a7551d
Andrew Millar <andrew.millar@qa.com>
2018-02-06 14:44:32 +0000

    Generalise settings up and executing collection exercise
    Added a template sql file to use for new collection exercises
    Add the data for each ce individually now in set_up_ce_execution
    Generalise executing each collection exercise in environment.py

--- resources/database/collection_exercise_setup/blocks_201801_setup.sql
+++ resources/database/collection_exercise_setup/blocks_201801_setup.sql
@@ -1,5 +1,5 @@
 insert into survey.classifierType( classifiertypepk,classifiertypeselectorfk,classifiertype)
-values(4,17,'COLLECTION_EXERCISE');
+values(5,15,'COLLECTION_EXERCISE');
 
 insert into action.actionplan(id, actionplanpk, name, description, createdby, lastrundatetime)
 values('878c3462-766a-4cf6-8fb3-867d5124cc0f',3,'Bricks and Blocks Enrolment','Bricks and Blocks Enrolment','SYSTEM',now());
@@ -8,7 +8,7 @@ insert into action.actionplan(id, actionplanpk, name, description, createdby, la
 values('46a4cdc2-8995-4969-a02d-37f6be767ef4',4,'Bricks and Blocks','Bricks and Blocks','SYSTEM',now());
 
 insert into collectionexercise.casetypedefault(casetypedefaultpk, sampleunittypefk, actionplanid, survey_uuid)
-values(3,'B','878c3462-766a-4cf6-8fb3-867d5124cc0f','cb8accda-6118-4d3b-85a3-149e28960c54');
+values(5,'B','878c3462-766a-4cf6-8fb3-867d5124cc0f','9b6872eb-28ee-4c09-b705-c3ab1bb0f9ec');
 
 insert into collectionexercise.casetypedefault(casetypedefaultpk, sampleunittypefk, actionplanid, survey_uuid)
-values(5,'B','878c3462-766a-4cf6-8fb3-867d5124cc0f','9b6872eb-28ee-4c09-b705-c3ab1bb0f9ec');
+values(6,'BI','46a4cdc2-8995-4969-a02d-37f6be767ef4','9b6872eb-28ee-4c09-b705-c3ab1bb0f9ec');
--- set_up_ce_execution.py
+++ set_up_ce_execution.py
@@ -11,10 +11,11 @@ if __name__ == '__main__':
     logging.info("Adding initial data required for executing collection exercise")
     # Add data required to execute collection exercise
     database_controller.execute_rm_sql('resources/database/collection_exercise_setup/bricks_201801_setup.sql')
+    database_controller.execute_rm_sql('resources/database/collection_exercise_setup/bricks_201812_setup.sql')
     database_controller.execute_rm_sql('resources/database/collection_exercise_setup/blocks_201801_setup.sql')
     # Retrieve collection exercise for BRICKS 2018 and upload collection instrument for it
     bricks_2018_ce = collection_exercise_controller.get_collection_exercise('cb8accda-6118-4d3b-85a3-149e28960c54',
                                                                             '201801')
     ci_path = 'resources/collection_instrument_files/064_0001_201803.xlsx'
     collection_instrument_controller.upload_collection_instrument(bricks_2018_ce['id'], ci_path)
-    logging.info(f"Collection exercises {bricks_2018_ce['id']} can be executed")
+    logging.info("Required collection exercises can be executed")
