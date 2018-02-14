a3922da4eea441209d1e1ac76c1855952cbb3122
Jamie Cox <me@jcox.tech>
2018-02-14 13:03:19 +0000

    Add missing variable assignment

--- acceptance_tests/features/steps/view_collection_exercise_details_state.py
+++ acceptance_tests/features/steps/view_collection_exercise_details_state.py
@@ -16,5 +16,5 @@ def internal_user_views_2017_bres_collection_exercise(_):
 
 @then('the user is able to view the status of the collection exercise')
 def ce_details_state_is_displayed():
-    collection_exercise_details.get_status()
+    ce_state = collection_exercise_details.get_status()
     assert ce_state != ''
