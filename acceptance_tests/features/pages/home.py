b248e62d93a14e26eea6a8172e09d3b1ff0c4682
Adam Mann <manny160389@hotmail.com>
2018-02-15 16:13:06 +0000

    First attempt at inbox tests

--- acceptance_tests/features/pages/home.py
+++ acceptance_tests/features/pages/home.py
@@ -9,3 +9,10 @@ def go_to():
 def click_surveys_link():
     link = browser.find_by_name('surveys-link')
     link.click()
+
+def verify_messages_link_present():
+    messages_link = browser.find_by_id('messages-header-link')
+    if messages_link:
+        return True
+    else:
+        return False
