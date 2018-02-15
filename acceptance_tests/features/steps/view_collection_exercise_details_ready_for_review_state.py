e4c6c32092fdfc6835a097e9586e397bd513fc39
Jamie Cox <me@jcox.tech>
2018-02-15 12:48:39 +0000

    Rename to remove abbreviations

--- acceptance_tests/features/steps/view_collection_exercise_details_ready_for_review_state.py
+++ acceptance_tests/features/steps/view_collection_exercise_details_ready_for_review_state.py
@@ -4,14 +4,14 @@ from acceptance_tests.features.pages import collection_exercise_details
 
 
 @given('the 201809 collection exercise for the RSI survey is Scheduled')
-def qifdi_201803_exists_and_scheduled_displayed(_):
+def rsi_201809_exists_and_scheduled_displayed(_):
     collection_exercise_details.go_to('RSI', '1809')
     ce_state = collection_exercise_details.get_status()
     assert ce_state == 'Scheduled'
 
 
 @given('the 201810 collection exercise for the RSI survey is Scheduled')
-def qifdi_201803_exists_and_scheduled_displayed(_):
+def rsi_201810_exists_and_scheduled_displayed(_):
     collection_exercise_details.go_to('RSI', '1810')
     ce_state = collection_exercise_details.get_status()
     assert ce_state == 'Scheduled'
