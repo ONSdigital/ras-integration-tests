02a172c9407599e973fbcf7b0782426206448672
Jamie Cox <me@jcox.tech>
2018-02-17 12:32:28 +0000

    Move steps file into steps directory

--- acceptance_tests/features/steps/view_collection_exercise_scheduled_state.py
+++ acceptance_tests/features/steps/view_collection_exercise_scheduled_state.py
@@ -10,7 +10,7 @@ def internal_user_views_qbs(_):
 
 @then('the status of the collection exercise is listed as Scheduled')
 def survey_ce_state_is_scheduled(_):
-    row = collection_exercise.get_table_row_by_period('201803')
+    row = collection_exercise.get_table_row_by_period('1803')
     assert row['state'] == 'Scheduled'
 
 
