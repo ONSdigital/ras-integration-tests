37013196b3b5397bdf0d2670544a5fffa878a0cd
Ben Jefferies <benjefferies@echosoft.uk>
2018-02-19 12:42:14 +0000

    Increase timeout and cache images

--- wait_until_services_up.py
+++ wait_until_services_up.py
@@ -18,7 +18,7 @@ def retry_if_http_error(exception):
     return isinstance(exception, RequestException) or isinstance(exception, HealthCheckException)
 
 
-@retry(retry_on_exception=retry_if_http_error, wait_fixed=10000, stop_max_delay=400000, wrap_exception=True)
+@retry(retry_on_exception=retry_if_http_error, wait_fixed=10000, stop_max_delay=600000, wrap_exception=True)
 def check_services():
     for port in [Config.ACTION_SERVICE_PORT, Config.BACKSTAGE_SERVICE_PORT,
                  Config.CASE_SERVICE_PORT, Config.COLLECTION_EXERCISE_SERVICE_PORT,
