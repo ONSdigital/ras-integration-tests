123ece288482637537118a9872a64cae09e80e60
Ben Jefferies <benjefferies@echosoft.uk>
2018-02-19 11:19:22 +0000

    Add logging if selenium failure

--- acceptance_tests/features/environment.py
+++ acceptance_tests/features/environment.py
@@ -1,6 +1,7 @@
 from logging import getLogger
 import time
 
+from selenium.common.exceptions import WebDriverException
 from structlog import wrap_logger
 
 from acceptance_tests import browser
@@ -21,12 +22,15 @@ def after_all(context):
 
 
 def before_all(context):
-    database_controller.execute_rm_sql('resources/database/database_reset_rm.sql')
-    database_controller.reset_ras_database()
-    authentication.signed_in_internal(context)
-    execute_collection_exercises()
-    register_respondent(survey_id='cb8accda-6118-4d3b-85a3-149e28960c54', period='201801')
-    sign_out_internal.sign_out()
+    try:
+        database_controller.execute_rm_sql('resources/database/database_reset_rm.sql')
+        database_controller.reset_ras_database()
+        authentication.signed_in_internal(context)
+        execute_collection_exercises()
+        register_respondent(survey_id='cb8accda-6118-4d3b-85a3-149e28960c54', period='201801')
+        sign_out_internal.sign_out()
+    except WebDriverException:
+        logger.exception('Failed to setup before running tests', html=browser.html)
 
 
 def before_scenario(_, scenario):
