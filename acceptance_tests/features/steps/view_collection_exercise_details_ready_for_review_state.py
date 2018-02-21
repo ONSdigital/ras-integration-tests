cd92cc98aa75f0098746531bc4f18143993cb0c2
Ben Jefferies <benjefferies@echosoft.uk>
2018-02-21 09:42:24 +0000

    Retry async state updates

--- acceptance_tests/features/steps/view_collection_exercise_details_ready_for_review_state.py
+++ acceptance_tests/features/steps/view_collection_exercise_details_ready_for_review_state.py
@@ -1,5 +1,8 @@
+import time
+
 from behave import given, when, then
 
+from acceptance_tests import browser
 from acceptance_tests.features.pages import collection_exercise, collection_exercise_details
 
 
@@ -65,4 +68,11 @@ def load_collection_instruments(_):
 @then('the status of the collection exercise is Ready for Review')
 def ce_details_state_is_ready_for_review(_):
     ce_state = collection_exercise_details.get_status()
+    # Status updated async so wait until updated
+    for i in range(3):
+        if ce_state == 'Ready for Review':
+            break
+        browser.reload()
+        ce_state = collection_exercise_details.get_status()
+        time.sleep(1)
     assert ce_state == 'Ready for Review'
