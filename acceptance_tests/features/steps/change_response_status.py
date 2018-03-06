3c51053e4df1d36ab1d8587a3c0021a6b4de348e
liamtoozer <liam.toozer@gmail.com>
2018-03-06 09:05:21 +0000

    Use page object for entering IAC

--- acceptance_tests/features/steps/change_response_status.py
+++ acceptance_tests/features/steps/change_response_status.py
@@ -1,9 +1,9 @@
 from behave import when, given, then
 
-from acceptance_tests import browser
+from acceptance_tests.features.pages.add_survey import enter_enrolment_code, click_continue_button
 from acceptance_tests.features.pages.reporting_unit import get_unused_iac
-from acceptance_tests.features.steps.authentication import signed_in_internal, signed_in_respondent
 from acceptance_tests.features.pages import add_survey, change_response_status, surveys_history, reporting_unit
+from acceptance_tests.features.steps.authentication import signed_in_internal, signed_in_respondent
 
 
 @given('the survey for 49900000002 has been completed by phone')
@@ -37,9 +37,8 @@ def _enrol_respondent_with_ru_ref(_, ru_ref):
 
     signed_in_respondent(_)
     add_survey.go_to()
-    browser.driver.find_element_by_id('ENROLEMENT_CODE_FIELD').send_keys(enrolment_code)
-    browser.find_by_id('CONTINUE_BTN').click()
-    browser.find_by_id('CONFIRM_SURVEY_BTN').click()
+    enter_enrolment_code(enrolment_code)
+    click_continue_button()
 
 
 def _change_response_status(_, ru_ref):
