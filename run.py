6a49d922e650e2a6f00e4e94cc4aab45cd87c39f
dcdarrell9 <darrells_king@hotmail.com>
2018-03-21 15:26:43 +0000

    Changed to progress2

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from behave import __main__ as behave_executable
 
 if __name__ == '__main__':
 
-    behave_executable.main(args='--format progress system_tests/features')
-    behave_executable.main(args='--format progress acceptance_tests/features')
+    behave_executable.main(args='--format progress2 system_tests/features')
+    behave_executable.main(args='--format progress2 acceptance_tests/features')
