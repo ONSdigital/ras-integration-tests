c71f1e60827bd19bae18f77584044b511b62189d
Adam Mann <manny160389@hotmail.com>
2018-03-27 12:00:36 +0100

    Add missing line

--- common/browser_utilities.py
+++ common/browser_utilities.py
@@ -1,5 +1,6 @@
 from acceptance_tests import browser
 
+
 def is_text_present_with_retry(text, retries=3, delay=1):
     for attempt in range(retries):
         if browser.is_text_present(text, wait_time=delay):
