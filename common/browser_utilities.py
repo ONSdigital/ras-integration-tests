dd336d7bc8ffb71f7c4dea31b8e76207983fb1f0
Adam Mann <manny160389@hotmail.com>
2018-03-27 11:53:52 +0100

    Tidy code up further

--- common/browser_utilities.py
+++ common/browser_utilities.py
@@ -2,9 +2,7 @@ from acceptance_tests import browser
 
 def is_text_present_with_retry(text, retries=3, delay=1):
     for attempt in range(retries):
-        if not browser.is_text_present(text, wait_time=delay):
-            browser.reload()
-        else:
+        if browser.is_text_present(text, wait_time=delay):
             return True
-
+        browser.reload()
     return False
