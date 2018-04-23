30847e913f29c7a67e8d12b92024b79d0e4eba33
Ben Jefferies <benjefferies@echosoft.uk>
2018-04-23 09:11:58 +0100

    Don't truncate alembic

--- resources/database/database_reset_ras_ci.sql
+++ resources/database/database_reset_ras_ci.sql
@@ -1,6 +1,5 @@
 /* Clean ras CI DB */
 TRUNCATE ras_ci.survey CASCADE;
-TRUNCATE ras_ci.alembic_version CASCADE;
 TRUNCATE ras_ci.exercise CASCADE;
 TRUNCATE ras_ci.instrument_exercise CASCADE;
 TRUNCATE ras_ci.seft_instrument CASCADE;
