d028a0a49a6f6c86b6407fd4e6a2d611c1ea0890
Matt Innes <matt@madeye.com>
2018-05-02 16:11:54 +0100

    Added test for duplication

--- acceptance_tests/features/create_survey.feature
+++ acceptance_tests/features/create_survey.feature
@@ -15,4 +15,16 @@ Feature: As an internal user
     | survey_id | survey_title                 | survey_abbreviation | survey_legal_basis           |
     | 9999      | Test Survey                  | CREATE              | Statistics of Trade Act 1947 |
 
+  @us101-s02
+  Scenario Outline: Internal user is not able to create a new survey with a duplicate reference
+    Given the internal user is on the survey list page
+    And the survey with reference <survey_id> already exists
+    When the internal user has entered the create survey URL
+    And they enter the new survey details as reference <survey_id>, title <survey_title>, abbreviation <survey_abbreviation> and legal basis <survey_legal_basis>
+    Then they get an error page with the message 'Survey with ID <survey_id> already exists'
+
+    Examples: Test survey data
+    | survey_id | survey_title                 | survey_abbreviation | survey_legal_basis           |
+    | 9999      | Another Test Survey          | DUPLICATE           | Statistics of Trade Act 1947 |
+
 
--- acceptance_tests/features/steps/create_survey.py
+++ acceptance_tests/features/steps/create_survey.py
@@ -10,12 +10,20 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 
 @given('the internal user has entered the create survey URL')
+@when('the internal user has entered the create survey URL')
 def check_user_on_survey_create_page(_):
     survey.go_to_create()
     expected_title = "Create survey"
     assert expected_title in browser.title, "Unexpected page title {} ({} expected)".format(browser.title,
                                                                                             expected_title)
+@given('the survey with reference {survey_id} already exists')
+def check_survey_exists(_, survey_id):
+    survey.go_to()
+    surveys = survey.get_surveys()
 
+    matching = [s for s in surveys if s['id'] == survey_id]
+
+    assert len(matching) > 0, "Cannot find the survey with reference {}".format(survey_id)
 
 @when('they enter the new survey details as reference {survey_id}, title {survey_title}, abbreviation {'
       'survey_abbreviation} and legal basis {survey_legal_basis}')
@@ -50,3 +58,10 @@ def view_survey_details(_, survey_id, survey_title, survey_abbreviation, survey_
         survey_by_id['short_name'], survey_abbreviation)
     assert survey_by_id['legal_basis'] == survey_legal_basis, "Unexpected survey legal basis {} ({} expected)".format(
         survey_by_id['legal_basis'], survey_legal_basis)
+
+@then('they get an error page with the message \'{expected_error_message}\'')
+def check_error_page(_, expected_error_message):
+    actual_error_message = create_survey_form.save_error()
+
+    assert expected_error_message == actual_error_message,\
+        "Expected error message '{}' not found (found '{}'".format(expected_error_message, actual_error_message)
