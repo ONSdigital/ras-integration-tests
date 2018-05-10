44923b88f6166d4c8fc9c405c0247094ca24e35d
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-05-10 12:59:56 +0100

    Add wait for case to update

--- acceptance_tests/features/steps/display_trading_as_external.py
+++ acceptance_tests/features/steps/display_trading_as_external.py
@@ -1,11 +1,11 @@
 from behave import given, then, when
 
 from acceptance_tests import browser
+from acceptance_tests.features.environment import wait_for_case_to_update
 from acceptance_tests.features.pages import reporting_unit, change_response_status
 from acceptance_tests.features.pages.surveys_history import go_to_history_tab
 from acceptance_tests.features.pages.surveys_todo import go_to as go_to_todo
 from acceptance_tests.features.steps.authentication import signed_in_internal, signed_in_respondent
-from config import Config
 from controllers.case_controller import generate_new_enrolment_code
 from controllers.collection_exercise_controller import get_collection_exercise
 from controllers.party_controller import add_survey, get_party_by_email
@@ -37,13 +37,15 @@ def respondent_has_completed_survey(_):
 
     # Complete the survey by phone using internal UI
     ru_ref = '49900000007'
+    party_id = get_party_by_email('example@example.com')['id']
     signed_in_internal(_)
     reporting_unit.go_to(ru_ref)
     reporting_unit.click_data_panel('QBS')
     reporting_unit.click_change_response_status_link(ru_ref=ru_ref, survey='QBS', period='1809')
+    wait_for_case_to_update(party_id)
     change_response_status.update_response_status('COMPLETED_BY_PHONE')
 
-    # Sign the respondent and navigate
+    # Ensure respondent is signed in and navigate to history
     signed_in_respondent(_)
     go_to_history_tab()
 
@@ -51,7 +53,7 @@ def respondent_has_completed_survey(_):
 @then('the trading name should be displayed below the business name for every survey')
 def trading_as_name_is_displayed_below_business_name_in_history(_):
     assert 'PC LTD' in browser.driver.find_element_by_id('REPORTING_UNIT_DETAILS_49900000007').text,\
-        'could not find trading as name "PC LTD" in respondent history page'
+        'Could not find trading as name "PC LTD" in respondent history page'
 
 
 @given('a company does not have a separate "trading as" name')
@@ -62,12 +64,7 @@ def company_does_not_have_trading_as_name(_):
 @then('the "trading as" field should not appear on the RU details page')
 def trading_as_field_does_not_appear(_):
     assert 'Trading as:' not in browser.driver.find_element_by_id('REPORTING_UNIT_DETAILS_49900000008').text,\
-        'Found trading as for a business that does not have a trading as name in respondent todo page'
-
-
-def _get_party_id_of_respondent(respondent_email):
-    party_response = get_party_by_email(respondent_email)
-    return party_response['id']
+        'Found "trading as" for a business that does not have a trading as name in respondent todo page'
 
 
 def _get_last_QBS_collection_exercise_id():
@@ -76,4 +73,4 @@ def _get_last_QBS_collection_exercise_id():
 
 def _add_survey_for_ru_to_respondent(respondent_email, ru_ref, collection_exercise_id):
     enrolment_code = generate_new_enrolment_code(collection_exercise_id, ru_ref)['iac']
-    add_survey(_get_party_id_of_respondent(respondent_email), enrolment_code)
+    add_survey(get_party_by_email(respondent_email)['id'], enrolment_code)
