7bf783774210a4c072cb35c33bcbbfdea9f4a9c7
liamtoozer <liam.toozer@gmail.com>
2018-09-19 15:53:21 +0100

    Tidy up imports

--- acceptance_tests/features/steps/social_change_case_status.py
+++ acceptance_tests/features/steps/social_change_case_status.py
@@ -4,9 +4,8 @@ from urllib.parse import urlparse
 from behave import given, when, then
 
 from acceptance_tests.features.pages import social_search_by_postcode
-from acceptance_tests.features.pages.social_change_case_status import click_first_new_response_status_option, \
-    click_set_new_status, click_status_option, get_social_case_change_status_url
-from acceptance_tests.features.pages.social_view_case_details import click_change_status, get_detailed_case_status
+from acceptance_tests.features.pages import social_change_case_status
+from acceptance_tests.features.pages import social_view_case_details
 from controllers.case_controller import get_case_by_id
 
 
@@ -20,28 +19,28 @@ def sel_user_finds_case_details(context):
 
 @when('they change the response status')
 def change_response_status(context):
-    click_change_status()
-    new_status = click_first_new_response_status_option()
+    social_view_case_details.click_change_status()
+    new_status = social_change_case_status.click_first_new_response_status_option()
     context.new_status = new_status.text
-    click_set_new_status()
+    social_change_case_status.click_set_new_status()
 
 
 @then('the new status is to be saved against that case')
 @then('the new response status is to be displayed on the case details page')
 def new_case_status_is_saved(context):
-    assert context.new_status == get_detailed_case_status()
+    assert context.new_status == social_view_case_details.get_detailed_case_status()
 
 
 @given('the SEL user has selected to change the status')
 def sel_user_has_navigated_to_change_status(context):
     sel_user_finds_case_details(context)
-    click_change_status()
+    social_view_case_details.click_change_status()
 
 
 @when("the status is updated to '{new_status}'")
 def sel_user_changes_status_to(context, new_status):
-    click_status_option(new_status)
-    click_set_new_status()
+    social_change_case_status.click_status_option(new_status)
+    social_change_case_status.click_set_new_status()
 
 
 @then('any further communication from being triggered such as pre-notification/invitation/reminder is prevented')
@@ -55,18 +54,18 @@ def case_is_set_to_inactionable(context):
 @given('the SEL user has selected a new response status')
 def sel_user_selects_first_new_status(context):
     sel_user_finds_case_details(context)
-    click_change_status()
-    click_first_new_response_status_option()
+    social_view_case_details.click_change_status()
+    social_change_case_status.click_first_new_response_status_option()
 
 
 @when('they confirm the status change')
 def sel_user_confirms_status_change(context):
-    new_status = click_first_new_response_status_option()
+    new_status = social_change_case_status.click_first_new_response_status_option()
     context.new_status = new_status.text
-    click_set_new_status()
+    social_change_case_status.click_set_new_status()
 
 
 def _get_case_id_from_url():
-    url = get_social_case_change_status_url()
+    url = social_change_case_status.get_social_case_change_status_url()
     url_path = urlparse(url).path
     return path.split(url_path)[-1]
