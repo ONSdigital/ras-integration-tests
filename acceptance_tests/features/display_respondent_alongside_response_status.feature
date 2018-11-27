708806e857b7eebffaf04dfcd6e8fc06e13df00e
Ryan Grundy <ryan.grundy@ons.gov.uk>
2018-11-27 14:27:14 +0000

    New feature tests for response status

--- acceptance_tests/features/display_respondent_alongside_response_status.feature
+++ acceptance_tests/features/display_respondent_alongside_response_status.feature
@@ -5,22 +5,39 @@ Feature: Internal user can search for a respondent via email
   I need to be able to search for a registered respondent via their email
   So that I can easily find them when dealing with operational calls
 
-#
-#  Background: internal user is already signed in
-#    Given the respondent is signed into their account
 
-  @fixture.setup.data.enrolled.respondent
+  @fixture.setup.data.with.enrolled.respondent.user.and.internal.user.and.new.iac.and.collection.exercise.to.live
   @us211_01
   Scenario: Respondent field must be present on each Survey displayed on Reporting units for any given Reference
     Given an external user is enrolled onto a given survey
     And has not initiated any changes to the "Not started" status
     When an internal user navigates to Reporting units for that Reference
     Then no Respondent name should be displayed in the Respondent field
-#
-#  @fixture.setup.data.enrolled.respondent
-#  Scenario: Respondent field must be updated once an external user has completed a survey online
-#    Given an external user is enrolled onto a given survey
-#    And the status is set to Completed
-#    When an internal user navigates to Reporting Units for that reference
-#    Then the Respondent details must be displayed in the Respondent field
 
+  @fixture.setup.data.with.enrolled.respondent.user.and.internal.user.and.new.iac.and.collection.exercise.to.live
+  Scenario: Respondent field must be updated once an external user has completed a survey online
+    Given  a user has completed a survey online
+    And the status is set to Completed
+    When an internal user navigates to Reporting Units for that reference
+    Then the Respondent details must be displayed in the Respondent field
+
+  @fixture.setup.data.with.enrolled.respondent.user.and.internal.user.and.new.iac.and.collection.exercise.to.live
+    Scenario: Respondent field must be updated once an external user has completed a survey online
+      Given a user has partially completed a survey (In progress)
+      And  the status is set to "In progress"
+      When an internal user navigates to Reporting Units for that reference
+      Then no Respondent name should be displayed in the Respondent field
+
+  @fixture.setup.data.with.enrolled.respondent.user.and.internal.user.and.new.iac.and.collection.exercise.to.live
+    Scenario: Respondent field must be updated once an external user has completed a survey online
+      Given a user has completed a survey via the telephone
+      And the status is set to "Completed by phone"
+      When an internal user navigates to Reporting Units for that reference
+      Then no Respondent name should be displayed in the Respondent field
+
+  @fixture.setup.data.with.enrolled.respondent.user.and.internal.user.and.new.iac.and.collection.exercise.to.live
+    Scenario: Respondent field must be updated once an external user has completed a survey online
+      Given an internal user has set a given survey to "No longer required" for a given respondent
+      And the status is set to "No longer required"
+      When an internal user navigates to Reporting Units for the no longer required reference
+      Then no longer required should be displayed in the survey collection exercise
--- acceptance_tests/features/steps/display_respondent_alongside_response_status.py
+++ acceptance_tests/features/steps/display_respondent_alongside_response_status.py
@@ -1,46 +1,92 @@
 import logging
-from pprint import pprint
 
 import time
 from behave import given, when, then
 from structlog import wrap_logger
 
 from acceptance_tests import browser
-from acceptance_tests.features.pages import reporting_unit, collection_exercise_details, sign_in_respondent
-from acceptance_tests.features.steps.authentication import signed_in_respondent, signed_in_internal
+from acceptance_tests.features.pages import reporting_unit, collection_exercise_details, \
+    change_response_status, surveys_todo
+from acceptance_tests.features.pages.surveys_history import go_to_history_tab
+from acceptance_tests.features.steps.authentication import signed_in_internal, signed_in_respondent
 from acceptance_tests.features.steps.change_response_status import go_to_reporting_unit_page
-from config import Config
+from controllers.collection_exercise_controller import map_ce_status
 
 logger = wrap_logger(logging.getLogger(__name__))
 
 
-@given('an external user is enrolled onto a given survey')
+@given('a user has completed a survey online')
 def enrolling_onto_survey(context):
-    enrolled_respondent_sign_in(context)
+    signed_in_respondent(context)
     browser.click_link_by_id('access_survey_button_1')
     collection_exercise_details.upload_collection_instrument(
         test_file='resources/collection_instrument_files/064_201803_0001.xlsx')
     browser.find_by_id('upload_survey_button').click()
 
 
-def enrolled_respondent_sign_in(context):
-    sign_in_respondent.go_to()
-    # Only attempt to sign in if not already signed in otherwise implicitly redirected to homepage
-    if '/sign-in' in browser.url:
-        browser.driver.find_element_by_id('username').send_keys(context.user_name)
-        browser.driver.find_element_by_id('inputPassword').send_keys(Config.RESPONDENT_PASSWORD)
-        time.sleep(40)
-        browser.find_by_id('sign_in_button').click()
+@given('an external user is enrolled onto a given survey')
+def enrolled_respondent_but_not_completed(context):
+    signed_in_respondent(context)
+
+
+@given('a user has completed a survey via the telephone')
+def user_completes_survey_by_telephone(context):
+    signed_in_internal(())
+    go_to_reporting_unit_page(context)
+    reporting_unit.click_data_panel(context.short_name)
+    reporting_unit.click_change_response_status_link(survey=context.short_name, period=context.period)
+    mapped_status = map_ce_status('Completed by phone')
+    change_response_status.update_response_status(mapped_status)
+
+
+@given('the status is set to "Completed by phone"')
+def survey_is_set_to_completed_by_telephone(context):
+    signed_in_respondent(context)
+    go_to_history_tab()
+    assert browser.find_by_text('Completed by phone')
+
+
+@given('the status is set to "No longer required"')
+def survey_set_to_longer_required(context):
+    signed_in_respondent(context)
+    go_to_history_tab()
+    assert browser.find_by_text('No longer required')
+
+
+@given('an internal user has set a given survey to "No longer required" for a given respondent')
+def survey_is_set_to_no_longer_required(context):
+    signed_in_internal(())
+    go_to_reporting_unit_page(context)
+    reporting_unit.click_data_panel(context.short_name)
+    reporting_unit.click_change_response_status_link(survey=context.short_name, period=context.period)
+    mapped_status = map_ce_status('No longer required')
+    change_response_status.update_response_status(mapped_status)
+
+
+@given('a user has partially completed a survey (In progress)')
+def respondent_partially_completes_survey(context):
+    signed_in_respondent(context)
+    browser.click_link_by_id('access_survey_button_1')
+    browser.click_link_by_id('download_survey_button')
+    time.sleep(5)
+
+
+@given('the status is set to "In progress"')
+def survey_set_to_in_progress(_):
+    surveys_todo.go_to()
+    assert browser.find_by_text('Downloaded')
 
 
 @given('has not initiated any changes to the "Not started" status')
 def no_changes_have_been_made_to_collex(_):
-    pass
+    assert browser.find_by_text('Not started')
 
 
 @given('the status is set to Completed')
 def status_set_to_completed(_):
-    pass
+    surveys_todo.go_to()
+    go_to_history_tab()
+    assert browser.find_by_text('Complete')
 
 
 @when('an internal user navigates to Reporting units for that Reference')
@@ -51,6 +97,26 @@ def navigating_to_reporting_units(context):
     reporting_unit.click_change_response_status_link(survey=context.short_name, period=context.period)
 
 
+@when('an internal user navigates to Reporting Units for the no longer required reference')
+def collex_set_to_no_longer_required(context):
+    signed_in_internal(())
+    go_to_reporting_unit_page(context)
+    reporting_unit.click_data_panel(context.short_name)
+
+
+@then('no longer required should be displayed in the survey collection exercise')
+def no_longer_required_set_for_collex(_):
+    assert 'No longer required' in browser.html
+
+
 @then('no Respondent name should be displayed in the Respondent field')
-def respondent_name_is_not_displayed(context):
+def respondent_name_is_not_displayed(_):
     assert 'Respondent:' in browser.html
+    assert browser.find_by_id('completed-respondent')[0].value is ''
+
+
+@then('the Respondent details must be displayed in the Respondent field')
+def respondent_name_is_displayed(_):
+    assert 'Respondent:' in browser.html
+    respondent = browser.find_by_id('completed-respondent')[0].value
+    assert respondent == 'first_name last_name'
