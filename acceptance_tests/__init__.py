13f8bb18d13b6ff9e5284022519d7314910261c6
Andrew Torrance <Andrew.Torrance@Ons.Gov.uk>
2019-02-11 13:44:50 +0000

    Added chrome driver binary to path

--- acceptance_tests/__init__.py
+++ acceptance_tests/__init__.py
@@ -1,8 +1,10 @@
 import logging
 import os
 
+from distutils.util import strtobool
 from retrying import retry
 from selenium import webdriver
+import chromedriver_binary
 from splinter import Browser
 from structlog import configure
 from structlog.stdlib import LoggerFactory
@@ -19,8 +21,11 @@ def web_driver_connection_error(e):
 
 @retry(retry_on_exception=web_driver_connection_error, wait_fixed=1000, stop_max_attempt_number=30)
 def create_browser():
+
     if os.getenv('HEADLESS', 'True') == 'True':
+        chromedriver_binary.add_chromedriver_to_path()
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.headless = bool(strtobool(os.getenv('HEADLESS', 'True')))
         chrome_options.add_argument("--no-sandbox")
         return Browser('chrome', headless=True, options=chrome_options)
     else:
