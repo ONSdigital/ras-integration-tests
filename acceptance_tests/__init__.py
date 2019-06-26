c9d73f65d215433502710367351b03a35e8847a3
Andrew <andrew.torrance@ons.gov.uk>
2019-06-26 10:20:04 +0100

    Cleaned up logic selecting browser driver

--- acceptance_tests/__init__.py
+++ acceptance_tests/__init__.py
@@ -22,16 +22,14 @@ def web_driver_connection_error(e):
 def create_browser():
     driver_type = os.getenv('WEBDRIVER', 'chrome').lower()
     headless = os.getenv('HEADLESS', 'True') == 'True'
-    if driver_type == 'firefox':
-        return Browser('firefox', headless=headless)
-    else:
-        if headless:
-            chromedriver_binary.add_chromedriver_to_path()
-            chrome_options = webdriver.ChromeOptions()
-            chrome_options.add_argument("--no-sandbox")
-            return Browser('chrome', headless=True, options=chrome_options)
-        else:
-            return Browser('chrome')
+
+    if driver_type == 'chrome' and headless:
+        chromedriver_binary.add_chromedriver_to_path()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--no-sandbox")
+        return Browser(driver_type, headless=driver_type, options=chrome_options)
+
+    return Browser(driver_type, headless=headless)
 
 
 browser = create_browser()
