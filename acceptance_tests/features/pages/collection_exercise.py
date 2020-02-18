4b00c23617400bbb3e53affdffb81c23b8f58691
Jon Fox <jon.fox@ext.ons.gov.uk>
2020-02-18 16:30:03 +0000

    better expected condition for a link element

--- acceptance_tests/features/pages/collection_exercise.py
+++ acceptance_tests/features/pages/collection_exercise.py
@@ -100,5 +100,5 @@ def get_collection_exercise_created_banner():
 
 def click_collection_exercise_created_banner():
     link = WebDriverWait(browser.driver, 60).until(
-        EC.presence_of_element_located((By.ID, 'newly_created_ce_link')))
+        EC.element_to_be_clickable((By.ID, 'newly_created_ce_link')))
     link.click()
